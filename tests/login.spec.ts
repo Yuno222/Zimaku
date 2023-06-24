1ca13b60d614f01861aa75170e8e39d1f67722e1
yunosuke <yuu0209ide@gmail.com>
2023-06-24 16:02:44 +0900

    edit

---
+++ tests/login.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('ゆずるね宣言する', async ({ page }) => {
+  await page.goto('https://mineo.jp/');
+  const page1Promise = page.waitForEvent('popup');
+  await page.locator('#default-header').getByRole('link', { name: 'マイページ' }).click();
+  const page1 = await page1Promise;
+  await page1.getByPlaceholder('eoID').fill('yunosuke135');
+  await page1.getByRole('button', { name: '次へ' }).click();
+  await page1.getByPlaceholder('eoIDパスワード').fill('pikapika29');
+  await page1.getByRole('button', { name: 'ログイン' }).click();
+  await page1.context().storageState({path: "login_state.json"})
+  // await page1.locator("#MineoSwitchSubmit").click();
+  // await page1.getByRole('button', { name: '節約ON' }).click();
+  // await page1.getByRole('button', { name: 'ゆずるね。宣言待ち' }).click();
+});
